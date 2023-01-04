0624ce4532446dd8a323694fd48147b1ecacd734
beming-dev <mingfordev@gmail.com>
2023-01-04 23:01:04 +0900

    Initialization

---
+++ chakra/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: "#000000",
+};
+
+const theme = extendTheme({ colors });
+
+export default theme;
---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import theme from "../chakra/theme";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
