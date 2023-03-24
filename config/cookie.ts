e24b8b313928ede9cb04cbd1c835f3af2250806e
beming-dev <mingfordev@gmail.com>
2023-03-24 19:51:32 +0900

    Update session option

--- config/cookie.ts
+++ config/cookie.ts
@@ -1,5 +1,8 @@
 const session_option = {
   cookieName: "diary-cookie",
+  cookieOptions: {
+    secure: false,
+  },
   password: process.env.COOKIE_PW || "XGEgJ6KQP4bjLeq4IWAbTIGMxxfjyDNoY",
   duration: 80000,
 };
